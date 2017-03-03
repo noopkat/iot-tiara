17ff9fb027466612da6040316e839afb237ec2a2
Suz Hinton <suz.hinton@gmail.com>
2017-03-03 13:12:59 -0500

    add IoT Hub sample

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const IRC = require('irc-framework');
+const IotClient = require('azure-iothub').Client;
+const IotMessage = require('azure-iot-common').Message;
 
 const bot = new IRC.Client();
+
 bot.connect({
     host: 'irc.chat.twitch.tv',
     port: 6667,
@@ -14,5 +17,32 @@ bot.on('registered', () => {
   channel.join();
   bot.matchMessage(/^gday/i, (event) => {
     event.reply('hello, '+ event.nick);
+    sendToDevice();
   });
 });
+
+function sendToDevice() {
+  const iotClient = IotClient.fromConnectionString(process.env.IOT_CONN_STRING);
+
+  iotClient.open(function (err) {
+    if (err) {
+      console.error('Could not connect: ' + err.message);
+    } else {
+      console.log('Client connected'); 
+      const data = JSON.stringify(1);
+      const message = new IotMessage(data);
+      console.log('Sending message: ' + message.getData());
+      iotClient.send(process.env.IOT_DEVICE_ID, message, printResultFor('send'));
+    }
+  });
+}
+
+function printResultFor(op) {
+  return function printResult(err, res) {
+    if (err) {
+      console.log(op + ' error: ' + err.toString());
+    } else {
+      console.log(op + ' status: ' + res.constructor.name);
+    }
+  };
+}
